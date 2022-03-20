bef0e50a678e53e54a63e59dfb541ccd517b5e79
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2022-03-20 08:25:08 +0000

    [コンフリクト修正]

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,5 +1,5 @@
 class Public::CustomersController < ApplicationController
-  before_action :authenticate_end_user!, except: [:top, :about]
+  before_action :authenticate_customers!, except: [:top, :about]
 
   def show
   end
